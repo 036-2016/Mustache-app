020dd680e11905ebb3d59efcb4e6e3f9414fa593
036-2016 <78303598+036-2016@users.noreply.github.com>
2023-10-22 21:08:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(400,450);
+    canvas.center();
+   }
+
+function draw()
+{
+    
+}
+
+function take_snapshot()
+{
+    save('mustache_img.png');
+}
